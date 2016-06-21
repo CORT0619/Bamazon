65df62d8f8d22fd9652cdf5f45dbb6785556a74b
Crystal <cortney0215@yahoo.com>
2016-06-21 10:37:33 -0400

    added spacing

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -20,9 +20,7 @@ beginApp();
 
 function beginApp(){
 
-	//var promise = new Promise(function(resolved, rejected){
 	return new Promise(function(resolved, rejected){
-
 
 		connection.query('SELECT * FROM products', function (err, rows, fields){
 
@@ -40,229 +38,110 @@ function beginApp(){
 			resolved();
 
 		});
-		//viewAllProducts();
 	});	
-/*
-		console.log(result);
-		if(result == true){
-			resolved();
-		} else {
-			rejected();
-		}
-	});
-
-
-	/*promise.then(function(){
-
-		inquirer.prompt([{
-
-			name: "prodID",
-			type: "input",
-			message: "\nWhat is the id of the product you would like to buy?",
-			validate: function(val){
-
-				if(isNaN(val) == false){
-
-		   			return true;
-
-		   		} else {
-
-		   			console.log("\nInvalid product id. Please try again.");
-		   			return false;
-		   		}
-		   	}	
-		}, {
-			name: "quantity",
-			type: "input",
-			message: "\nHow many would you like to purchase?",
-			validate: function(val){
-				if(isNaN(val) == false){
-
-		   			return true;
-
-		   		} else{
-
-		   			console.log("\nInvalid quantity. Please try again.");
-		   			return false;
-		   		}
-			}
-
-		   }]).then(function(answers){
-
-	  			connection.query('SELECT StockQuantity, Price, DepartmentName FROM products WHERE ItemID=' + answers.prodID, 
-
-	   				function(err, rows, fields){
-
-	   				if(err) throw err;
-
-	   				var result = parseInt(rows[0].StockQuantity);
-	   				var price = parseFloat(rows[0].Price);
-
-	   				var userQuan = parseInt(answers.quantity);
-	   				var department = rows[0].DepartmentName;
-
-	   				if(result >= userQuan){
-
-	   					var total = (userQuan * price).toFixed(2);
-
-	   					console.log("Order Total: $" + total);
-
-	   					result -= userQuan;
-
-	   					var query = 'UPDATE products INNER JOIN departments ON products.DepartmentName = departments.DepartmentName SET products.StockQuantity=' + result + ', departments.totalSales= departments.totalSales +' + total + ' WHERE products.ItemID=' + answers.prodID + " AND departments.DepartmentName='" + department + "'";
-
-	   					//console.log(query);
-
-	   					connection.query(query, function(err, result){
-
-	   						if(err) throw err;
-
-	   						console.log("Order successfully placed!");
-
-	   					});
-
-	   				} else{
-
-	   					console.log("Insufficient quantity.");
-	   					return false;
-	   				}
-
-	   					inquirer.prompt({
-							name: 'whatsNext',
-							type: 'list',
-							message: 'What would you like to do next?',
-							choices: ['   Continue', '   Exit']
-
-						}).then(function(answer){
-
-							switch(answer.whatsNext){
-								case '   Continue':
-									beginApp();
-								break;
-
-								case '   Exit':
-									exitApp();
-								break;
-							}
-						});
-
-	   			});
-		   });
-
-
-	}, function(){
-
-		return false;
-	});*/
-
 }
 
-//function askCust(){
+beginApp().then(function(){
 
-	beginApp().then(function(){
+	inquirer.prompt([{
 
-		console.log("here");
+	name: "prodID",
+	type: "input",
+	message: "\nWhat is the id of the product you would like to buy?",
+	validate: function(val){
 
-		inquirer.prompt([{
+		if(isNaN(val) == false){
 
-		name: "prodID",
-		type: "input",
-		message: "\nWhat is the id of the product you would like to buy?",
-		validate: function(val){
+   			return true;
 
-			if(isNaN(val) == false){
+   		} else {
 
-	   			return true;
+   			console.log("\nInvalid product id. Please try again.");
+   			return false;
+   		}
+   	}	
+}, {
+	name: "quantity",
+	type: "input",
+	message: "\nHow many would you like to purchase?",
+	validate: function(val){
+		if(isNaN(val) == false){
 
-	   		} else {
+   			return true;
 
-	   			console.log("\nInvalid product id. Please try again.");
-	   			return false;
-	   		}
-	   	}	
-	}, {
-		name: "quantity",
-		type: "input",
-		message: "\nHow many would you like to purchase?",
-		validate: function(val){
-			if(isNaN(val) == false){
+   		} else{
 
-	   			return true;
+   			console.log("\nInvalid quantity. Please try again.");
+   			return false;
+   		}
+	}
 
-	   		} else{
+   }]).then(function(answers){
 
-	   			console.log("\nInvalid quantity. Please try again.");
-	   			return false;
-	   		}
-		}
+			connection.query('SELECT StockQuantity, Price, DepartmentName FROM products WHERE ItemID=' + answers.prodID, 
 
-	   }]).then(function(answers){
+				function(err, rows, fields){
 
-  			connection.query('SELECT StockQuantity, Price, DepartmentName FROM products WHERE ItemID=' + answers.prodID, 
+				if(err) throw err;
 
-   				function(err, rows, fields){
+				var result = parseInt(rows[0].StockQuantity);
+				var price = parseFloat(rows[0].Price);
 
-   				if(err) throw err;
+				var userQuan = parseInt(answers.quantity);
+				var department = rows[0].DepartmentName;
 
-   				var result = parseInt(rows[0].StockQuantity);
-   				var price = parseFloat(rows[0].Price);
+				if(result >= userQuan){
 
-   				var userQuan = parseInt(answers.quantity);
-   				var department = rows[0].DepartmentName;
+					var total = (userQuan * price).toFixed(2);
 
-   				if(result >= userQuan){
+					console.log("\nOrder Total: $" + total);
 
-   					var total = (userQuan * price).toFixed(2);
+					result -= userQuan;
 
-   					console.log("Order Total: $" + total);
+					var query = 'UPDATE products INNER JOIN departments ON products.DepartmentName = departments.DepartmentName SET products.StockQuantity=' + result + ', departments.totalSales= departments.totalSales +' + total + ' WHERE products.ItemID=' + answers.prodID + " AND departments.DepartmentName='" + department + "'";
 
-   					result -= userQuan;
 
-   					var query = 'UPDATE products INNER JOIN departments ON products.DepartmentName = departments.DepartmentName SET products.StockQuantity=' + result + ', departments.totalSales= departments.totalSales +' + total + ' WHERE products.ItemID=' + answers.prodID + " AND departments.DepartmentName='" + department + "'";
+					connection.query(query, function(err, result){
 
-   					//console.log(query);
+						if(err) throw err;
 
-   					connection.query(query, function(err, result){
+						console.log("\nOrder successfully placed!");
 
-   						if(err) throw err;
-
-   						console.log("Order successfully placed!");
-
-   					});
-
-   				} else{
-
-   					console.log("Insufficient quantity.");
-   					return false;
-   				}
-
-   					inquirer.prompt({
-						name: 'whatsNext',
-						type: 'list',
-						message: 'What would you like to do next?',
-						choices: ['   Continue', '   Exit']
-
-					}).then(function(answer){
-
-						switch(answer.whatsNext){
-							case '   Continue':
-								beginApp();
-							break;
-
-							case '   Exit':
-								exitApp();
-							break;
-						}
 					});
 
-   			});
-	   });
+				} else{
 
-	}, function(){
+					console.log("Insufficient quantity.");
+					return false;
+				}
 
-		connection.end();
-	});
-//}
+				inquirer.prompt({
+					name: 'whatsNext',
+					type: 'list',
+					message: '\nWhat would you like to do next?',
+					choices: ['   Continue', '   Exit']
+
+				}).then(function(answer){
+
+					switch(answer.whatsNext){
+						case '   Continue':
+							beginApp();
+						break;
+
+						case '   Exit':
+							exitApp();
+						break;
+					}
+				});
+
+			});
+   });
+
+}, function(){
+
+	connection.end();
+});
+
 
 function viewAllProducts(){
 
